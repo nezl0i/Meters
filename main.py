1aca3c464c07cc24bab49ac15fe233041d08976c
nezl0i <nezl0in@gmail.com>
2021-12-21 06:47:54 +0300

    change gitignore

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from meters import Meters
 
 
-m = Meters()
-m.set_db_name("meters.db")
+m = Meters(db_name="meters.db", path='report', db_create=False)
+# m.set_db_name("meters.db")
 # m.db_create()
-# m.parse_excel(path='report', insert=True)
+m.parse_excel(insert=False)
 print(m.all())
 # m.sql_qw2("Славянские ЭС")
 # m.sql_qw(filial_firstname='Славянские ЭС', device='МИР', parameter='Процент', date='17.12.2021')
